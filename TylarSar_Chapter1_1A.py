e83d2b9ec5ee4aad529c248952262eac80f8b28f
Tylar Sar <tylarsar1@gmail.com>
2024-05-15 22:51:06 -0400

    TylarSar_Chapter1_1A Update
    Comments and clarification for constraints

--- TylarSar_Chapter1_1A.py
+++ TylarSar_Chapter1_1A.py
@@ -11,19 +11,25 @@ def ticketBooth():
 
     while ticketsAvailable > 0:
         print("Tickets available ", ticketsAvailable)
-        purchasedTickets = int(input("Desired number of tickets (max 4) "))
+        purchasedTickets = int(input("Desired number of tickets (max 4): "))
 
-
+#if statements are for constraints if the user enters 0 or negative numbers, more than 4 tickets,
+#or more than the available tickets (these are in order)
         if purchasedTickets <= 0:
+            print("You cannot purchase 0 tickets.")
             continue
         if purchasedTickets > 4:
+            print("You cannot purchase over 4 tickets.")
             continue
         if purchasedTickets > ticketsAvailable:
+            print("Available tickets is lower than the amount desired to buy.")
             continue
 
+#the total tickets available subtracted by purchased tickets, add the total buyers.
         ticketsAvailable -= purchasedTickets
         buyers += 1
 
+#display the total buyers at the end
     print("Total buyers ", buyers)
 
 def main():
